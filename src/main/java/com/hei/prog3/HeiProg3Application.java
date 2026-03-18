831632e381622e52504eb71f1b42f917d54a301f
Mathieu <hei.tafita.2@gmail.com>
2026-03-18 14:05:34 +0300

    spring setup

---
+++ src/main/java/com/hei/prog3/HeiProg3Application.java
@@ -0,0 +1,13 @@
+package com.hei.prog3.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeiProg3ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeiProg3ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hei/prog3/api/HeiProg3ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hei.prog3.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeiProg3ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
